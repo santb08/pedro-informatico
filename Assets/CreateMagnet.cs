df674599cd774dab31e61e14186ff0e7879628d8
Santiago <scb9908@gmail.com>
2020-02-27 20:29:26 -0500

    Created magnetism

---
+++ Assets/CreateMagnet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMagnet : MonoBehaviour
+{
+    [SerializeField] private GameObject magnetPrefab;
+    private GameObject magnetObject;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J) && magnetObject == null)
+        {
+            magnetObject = Instantiate(magnetPrefab, transform);
+        }
+        if (Input.GetKeyUp(KeyCode.J) && magnetObject != null)
+        {
+            Destroy(magnetObject);
+        }
+    }
+}
