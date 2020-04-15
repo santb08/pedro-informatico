ce571ba5e53f6460ac870d4cc637a7da29c48df8
Santiago Betancur <scb9908@gmail.com>
2020-04-14 21:10:24 -0500

    Tutorial

---
+++ Assets/Scripts/My Scripts/RenderTutorial.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RenderTutorial : MonoBehaviour
+{
+    public LayerMask layerPlayer;
+    public Text txtTutorial;
+    public string hint;
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        txtTutorial.text = hint;
+    }
+    
+    void OnTriggerExit2D(Collider2D collision) {
+        txtTutorial.text = "";
+    }
+}
