a3c8bee3a359522226c201bd5a8802abb8c86a99
JQartas <jcuartas0611@gmail.com>
2020-04-15 07:05:39 -0500

    Puntaje y obstaculos en niveles

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI score;
+    private string ans = "";
     private void Start()
     {
         GameManager.CurrentLevel = 1;
+        foreach (var question in GameManager.GetQuestions())
+        {
+            ans += "- "+question.Answers[question.correctAnswerIndex] + "\n";
+        }
+        score.text = "Puntuación: "+GameManager.GetCurrentScore() + "\n Respuestas correctas: \n" + ans;
     }
     public void GoToMenu()
     {
